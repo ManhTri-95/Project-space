ad31003726cf87d04ec681e26586be165d455b42
MSI\Modern 14 <bootrancntt@gmail.com>
2021-01-10 20:37:19 +0700

    initialization project

---
+++ ws_space/src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="hero">
+        <header class="header">
+            <div class="header__container">
+                <div class="logo">
+                     <img src="../assets/logo.png" alt="" />
+                </div>
+                <div class="header__right">
+                    <nav class="nav-menubar">
+                        <ul class="menubar">
+                            <li class="menuitem"><a href="#">Home</a></li>
+                            <li class="menuitem"><a href="#">Templates</a></li>
+                            <li class="menuitem"><a href="#">Price</a></li>
+                            <li class="menuitem"><a href="#">Help</a></li>
+                        </ul>
+                    </nav>
+                    <a href="#" class="btn btn-small outline-primary">Get In Touch</a>
+                </div>
+            </div>
+        </header>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Header',
+   
+}
+</script>
+
+<style scoped>
+    @import './../assets/css/header.css';
+    @import './../assets/css/button.css';
+</style>
---
+++ ws_space/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+export default {
+  name: 'HelloWorld',
+  components: {
+    Header,
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @import '../assets/css/header.css';
+</style>
